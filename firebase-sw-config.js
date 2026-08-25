41f1c7c15648a5a5f5d43c6575ffdf992baa28ae
App <luciano.barrera097@gmail.com>
2026-08-25 09:09:48 -0300

    Add files via upload

---
+++ firebase-sw-config.js
@@ -0,0 +1,10 @@
+/* Config mínima para el Service Worker (FCM en segundo plano).
+   Debe coincidir con firebase-config.js de la app. */
+self.__FIREBASE_CONFIG__ = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_PROYECTO.firebaseapp.com",
+  projectId: "TU_PROYECTO",
+  storageBucket: "TU_PROYECTO.appspot.com",
+  messagingSenderId: "TU_SENDER_ID",
+  appId: "TU_APP_ID"
+};
